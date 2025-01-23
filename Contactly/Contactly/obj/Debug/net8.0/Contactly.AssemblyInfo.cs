635a4f605c2b50b303a33915c9c7112ddfaa501b
Tayyeb-B <tayyebbilal574@gmail.com>
2025-01-23 12:24:36 +0500

    git ignore

--- Contactly/Contactly/obj/Debug/net8.0/Contactly.AssemblyInfo.cs
+++ Contactly/Contactly/obj/Debug/net8.0/Contactly.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Contactly")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e054ef8c7b3a339db4a4da6f30653c625c22603")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2756bd5f3f551806900464b4ddb0d06ffba1b515")]
 [assembly: System.Reflection.AssemblyProductAttribute("Contactly")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Contactly")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
